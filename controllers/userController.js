db592cd734daeecf05365ab2d0b36eb4401c7356
Hadi-Karnib <hadikarnib03@gmail.com>
2025-08-10 14:34:54 +0300

    FIX: return ivalid user or unvalid password

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,9 +39,11 @@ export const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email });
-  if (!user || !(await bcrypt.compare(password, user.password)))
-    return res.status(401).json({ message: "Invalid email or password" });
-
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  if (!(await bcrypt.compare(password, user.password)))
+    return res.status(401).json({ message: `Invalid password for ${email}` });
   generateToken(res, user._id);
   res.status(200).json({ _id: user._id, name: user.name, email: user.email });
 };
